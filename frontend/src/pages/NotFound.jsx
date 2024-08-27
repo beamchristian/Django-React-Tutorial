9b833e810fb001763f357b491bfed0c2cfeb9322
ckbeamsoftware <beamchristian@yahoo.com>
2024-08-26 20:57:55 -0400

    NotFound

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function NotFound() {
-  return <div>Not Found</div>;
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The page you're looking for does not exist!. 😔</p>
+    </div>
+  );
 }
 
 export default NotFound;
